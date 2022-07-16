0a857f6c60df5407f8717962d1beb4b615ec99e0
Maniash <solodovnikov.andrey99@mail.ru>
2022-07-17 01:28:48 +0300

    Redux-Toolkit
    createAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit"
+
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching())
     request("http://localhost:3001/heroes")
@@ -13,43 +15,24 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+export const heroesFetching = createAction('HEROES_FETCHING');
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: 'HEROES_FETCHED',
+//         payload: heroes
+//     }
+// }
+// Когда используем команду createAction, аргумент, который приходит createAction, автоматически переходит в поле с названием payload
+export const heroesFetched = createAction('HEROES_FETCHED');
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const filtersFetching = () => {
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
+export const filtersFetching = createAction('FILTERS_FETCHING');
 
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
+export const filtersFetched = createAction('FILTERS_FETCHED');
 
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
 
 // Такие конструкции позволяется делать Redux-Thunk
 // export const activeFilterChanged = (filter) => (dispatch) => {
@@ -61,23 +44,8 @@ export const filtersFetchingError = () => {
 //     }, 1000)
 // }
 
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
+export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED');
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
+export const heroCreated = createAction('HERO_CREATED');
 
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
+export const heroDeleted = createAction('HERO_DELETED');
--- src/store/index.js
+++ src/store/index.js
@@ -12,14 +12,6 @@ const stringMiddleware = (store) => (next) => (action) => {
     return next(action);
 }
 
-// const store = createStore(
-//     combineReducers({heroes, filters}),
-//     compose(
-//         applyMiddleware(ReduxThunk, stringMiddleware),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-// );
-
 const store = configureStore({
     reducer: {
         heroes,
